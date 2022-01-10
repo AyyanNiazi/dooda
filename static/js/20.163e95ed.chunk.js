fb1fe6a912e00f93afe32be0c50001476971f73f
Muhammad Arslan <dev_arslan@unblinked.com>
2022-01-10 17:33:59 +0500

    Updates

---
+++ static/js/20.163e95ed.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[20],{1324:function(n,c){},1371:function(n,c){},1373:function(n,c){},1397:function(n,c){},1399:function(n,c){},1412:function(n,c){},1430:function(n,c){},1432:function(n,c){}}]);
+//# sourceMappingURL=20.163e95ed.chunk.js.map
